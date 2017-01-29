4f0e19606f4252927abf97a31cc6463740d4b821
Stephen Koo <stephen.koo@inlight.com.au>
2017-01-29 18:43:09 +1100

    Apply `sass-loader` to webpack

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -109,6 +109,7 @@ module.exports = {
         exclude: [
           /\.html$/,
           /\.(js|jsx)$/,
+          /\.scss$/,
           /\.css$/,
           /\.json$/,
           /\.svg$/
@@ -137,6 +138,11 @@ module.exports = {
       // "style" loader turns CSS into JS modules that inject <style> tags.
       // In production, we use a plugin to extract that CSS to a file, but
       // in development "style" loader enables hot editing of CSS.
+      {
+        test: /\.scss$/,
+        include: paths.appSrc,
+        loaders: ['style', 'css', 'sass']
+      },
       {
         test: /\.css$/,
         loader: 'style!css?importLoaders=1!postcss'
